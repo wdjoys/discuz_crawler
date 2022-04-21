427b61ec75c5e9390ce5f885f80a7063776832d9
xiaocao <wdjoys@hotmail.com>
2022-04-21 18:07:44 +0800

    0421

--- discuz/discuz/settings.py
+++ discuz/discuz/settings.py
@@ -30,7 +30,7 @@ BOT_NAME = "discuz"
 SPIDER_MODULES = ["discuz.spiders"]
 NEWSPIDER_MODULE = "discuz.spiders"
 
-LOG_LEVEL = "ERROR"
+LOG_LEVEL = "INFO"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
@@ -40,12 +40,12 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 50
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.1
+DOWNLOAD_DELAY = 0.01
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
